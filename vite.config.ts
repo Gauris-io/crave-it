acaa3233e89a5ebc0651e8fc0440a610715aaaf4
avanie-s <avaniesharma1806@gmail.com>
2026-02-09 23:18:54 +0530

    added comment to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+  // Plugins for the vite build process
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
